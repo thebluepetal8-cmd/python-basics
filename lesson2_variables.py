55f36cda4f0d7246260200bad9a44148a2b7416d
thebluepetal8-cmd <thebluepetal8@gmail.com>
2026-05-22 21:42:40 +0530

    Create  lesson2_variables.py

---
+++ lesson2_variables.py
@@ -0,0 +1,19 @@
+# Lesson 2 - Variables and F-strings
+name = "Sun"
+university = "ABC school"
+course = "BCA AI and ML"
+year = 2026
+
+print("My name is", name)
+print("I study at", university)
+print("My course is", course)
+print("I will graduate in", year)
+
+# F-strings
+favourite_colour = "Black"
+favourite_subject = "English"
+age = 26
+
+print(f"My favourite colour is {favourite_colour}")
+print(f"My favourite subject is {favourite_subject}")
+print(f"I am {age} years old")
