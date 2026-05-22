af691ae1d53d1d078fcb906121dd78cd72889de7
thebluepetal8-cmd <thebluepetal8@gmail.com>
2026-05-22 21:43:12 +0530

    Create  lesson3_input.py

---
+++ lesson3_input.py
@@ -0,0 +1,8 @@
+# Lesson 3 - User Input
+name = input("What is your name? ").strip()
+university = input("Which university you are studying? ").strip()
+favourite_hobby = input("What is your favourite hobby? ").strip()
+
+print(f"Hi {name}!")
+print(f"You are studying at {university}.")
+print(f"And you love {favourite_hobby}.")
